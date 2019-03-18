7ac1500045628d57464a829059a012e46ccfca02
Mário Valney <mariovalney@gmail.com>
2019-03-18 16:32:06 -0300

    [Fix] Suporte a notificações com HTML

--- modules/dependence/includes/views/html-notice.php
+++ modules/dependence/includes/views/html-notice.php
@@ -9,5 +9,5 @@ defined( 'ABSPATH' ) || die( 'No script kiddies please!' );
 ?>
 
 <div class="notice notice-<?php echo esc_attr( $notice[1] ?? 'error' ); ?>">
-    <p><?php echo $notice[0]; ?></p>
+    <p><?php echo $notice[0]; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
 </div>
