9f7d604f84664553e3e180f03dd734287572f9cb
Mário Valney <mariovalney@gmail.com>
2019-04-29 12:59:24 -0300

    [Feat] BS info in Thank you page

--- modules/woocommerce/assets/js/frontend.js
+++ modules/woocommerce/assets/js/frontend.js
@@ -6,19 +6,30 @@ jQuery( document ).ready(
          * Element: Payment Method Checkbox
          * Event: Show form on checked
          */
-        $( 'body' ).on(
-            'change',
-            '[name="braspag_payment_method"]',
-            function(event) {
-                var code = $( this ).val();
+        $( 'body' ).on( 'change', '[name="braspag_payment_method"]', function(event) {
+            var code = $( this ).val();
 
-                if ( ! $( this ).prop( 'checked' ) || ! code ) {
-                    return;
-                }
-
-                $( '.braspag-method-label, .braspag-method-form' ).removeClass( 'active' );
-                $( '#payment-method-' + code + '-label, #braspag-payment-method-' + code + '-form' ).addClass( 'active' );
+            if ( ! $( this ).prop( 'checked' ) || ! code ) {
+                return;
             }
-        );
+
+            $( '.braspag-method-label, .braspag-method-form' ).removeClass( 'active' );
+            $( '#payment-method-' + code + '-label, #braspag-payment-method-' + code + '-form' ).addClass( 'active' );
+        } );
+
+        /**
+         * Element: .selectable-content
+         * Event: Select and focus on click
+         */
+        $( 'body' ).on( 'click', '.selectable-content', function(event) {
+            $( this ).focus();
+            $( this ).select();
+
+            if (typeof document.execCommand === 'undefined') {
+                return;
+            }
+
+            document.execCommand( 'copy' );
+        } );
     }
 );
--- modules/woocommerce/includes/templates/order-received.php
+++ modules/woocommerce/includes/templates/order-received.php
@@ -12,7 +12,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 
 <p class="woocommerce-thankyou-order-received-payment-data">
-    <?php esc_html_e( 'Your payment data:', VZR_TEXTDOMAIN ); ?>
+    <?php esc_html_e( 'Your payment data:', WCB_TEXTDOMAIN ); ?>
 </p>
 
 <ul class="woocommerce-thankyou-payment-details order_details">
@@ -25,17 +25,17 @@ if ( ! defined( 'ABSPATH' ) ) {
         <li class="woocommerce-order-overview__payment-method-name method-name">
             <?php esc_html_e( 'Installments:', WCB_TEXTDOMAIN ); ?>
             <strong>
-                <?php
-                if ( ! empty( $payment['Amount'] ) ) {
-                    $installment = $payment['Amount'] / $payment['Installments'] / 100;
-                    $installment = number_format( $installment, 2, ',', '' );
+            <?php
+            if ( ! empty( $payment['Amount'] ) ) {
+                $installment = $payment['Amount'] / $payment['Installments'] / 100;
+                $installment = number_format( $installment, 2, ',', '' );
 
-                    // translators: First is installments count and second is amount by installment
-                    echo esc_html( sprintf( __( '%1$s x R$%2$s' ), $payment['Installments'], $installment ) );
-                } else {
-                    echo esc_html( $payment['Installments'] );
-                }
-                ?>
+                // translators: First is installments count and second is amount by installment
+                echo esc_html( sprintf( __( '%1$s x R$%2$s' ), $payment['Installments'], $installment ) );
+            } else {
+                echo esc_html( $payment['Installments'] );
+            }
+            ?>
             </strong>
         </li>
     <?php endif; ?>
@@ -44,25 +44,40 @@ if ( ! defined( 'ABSPATH' ) ) {
         <li class="woocommerce-order-overview__payment-card-number card-number">
             <?php esc_html_e( 'Credit Card:', WCB_TEXTDOMAIN ); ?>
             <strong>
-                <?php
-                    esc_html( $payment['CreditCard']['CardNumber'] );
+            <?php
+                esc_html( $payment['CreditCard']['CardNumber'] );
 
-                if ( ! empty( $payment['CreditCard']['Brand'] ) ) {
-                    esc_html( ' (' . $payment['CreditCard']['Brand'] . ')' );
-                }
-                ?>
+            if ( ! empty( $payment['CreditCard']['Brand'] ) ) {
+                esc_html( ' (' . $payment['CreditCard']['Brand'] . ')' );
+            }
+            ?>
             </strong>
         </li>
     <?php endif; ?>
 
     <li class="woocommerce-order-overview__payment-card-number card-number">
         <?php esc_html_e( 'Status:', WCB_TEXTDOMAIN ); ?>
-        <strong><?php esc_html( wc_get_order_status_name( $status ) ); ?></strong>
+        <strong><?php esc_html_e( wc_get_order_status_name( $status ) ); ?></strong>
         <br>
-        <?php if ( $status === 'pending' ) : ?>
-            <div class="woocommerce-message">
-                <?php esc_html__( 'Your payment is pending for confirmation.', VZR_TEXTDOMAIN ); ?>
-            <span>
+        <?php if ( $status === 'on-hold' || $status === 'pending' ) : ?>
+            <?php esc_html_e( 'Your payment is pending for confirmation.', WCB_TEXTDOMAIN ); ?>
         <?php endif; ?>
     </li>
+
+    <?php if ( ! empty( $payment['Url'] ) && ! empty( $payment['BoletoNumber'] ) ) : ?>
+        <li class="woocommerce-order-overview__payment-bank-slip-url bank-slip-url">
+            <?php esc_html_e( 'Bank Slip:', WCB_TEXTDOMAIN ); ?>
+            <br>
+            <a href="<?php echo esc_url( $payment['Url'] ); ?>" class="woocommerce-button button" target="_blank" rel="noopener noreferrer">
+                <?php esc_html_e( 'Print bank slip', WCB_TEXTDOMAIN ); ?>
+            </a>
+            <?php if ( ! empty( $payment['DigitableLine'] ) ) : ?>
+                <span class="digitable-line">
+                    <?php esc_html_e( 'Digitable Line:', WCB_TEXTDOMAIN ); ?>
+                    <input type="text" class="selectable-content" value="<?php echo esc_html( $payment['DigitableLine'] ); ?>"
+                        title="<?php esc_html_e( 'Click to copy', WCB_TEXTDOMAIN ); ?>" readonly>
+                </span>
+            <?php endif; ?>
+        </li>
+    <?php endif; ?>
 </ul>
